5e35e98bbc9f078190d464974d29029065d1bdea
Maxim Baykevich <max.baikevich@gmail.com>
2020-04-29 17:06:48 +0300

    createStore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+  if(state === undefined) {
+    return 0;
+  }
+  switch (action.type) {
+   case 'INC':
+    return state + 1;
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+store.subscribe(()=> {
+  console.log(store.getState());
+});
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});

8a619faf306bfa2be457a980e24f93cef53534c7
Maxim Baykevich <max.baikevich@gmail.com>
2020-05-03 21:25:44 +0300

    bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import {inc, dec, rnd} from './actions'
+import * as actions from './actions'
 
 
 const store = createStore(reducer);
+const {dispatch } = store;
+
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+
+
 document
   .getElementById('inc')
-  .addEventListener('click', () => {
-    store.dispatch(inc());
-  });
+  .addEventListener('click', inc);
 
   document
   .getElementById('dec')
-  .addEventListener('click', () => {
-    store.dispatch(dec());
-  });
+  .addEventListener('click',dec);
 
   document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payLoad = Math.floor(Math.random()*10);
-    store.dispatch(rnd(payLoad));
+    rnd(payLoad);
   });
 
   const update =() => {
